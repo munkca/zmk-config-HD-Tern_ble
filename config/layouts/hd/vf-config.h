05259b529a308a5c51c75b42094b10ff5fc4bb2d
munkca <137710061+munkca@users.noreply.github.com>
2025-09-14 09:03:32 +0800

    Update vf-config.h

--- config/layouts/hd/vf-config.h
+++ config/layouts/hd/vf-config.h
@@ -16,7 +16,7 @@
 /*      Base (alpha) Layer Hands Down Vibranium-vf
                     34 keys (core)
  ╭─────────────────────╮ ╭──────────────────────╮
- │      W   M   G   X  │ │  -+  .:  /*   J      │
+ │      W   M   G   X  │ │   "  .:  ?!   J      │
  │  S   C   N   T   K  | |  ,;   A   E   I   H  │
  │  F   P   L   D      │ │       U   O   Y   B  │
  ╰──────────╮  BSP  R  │ │  SPC RET  ╭──────────╯
@@ -27,12 +27,12 @@
 #define HD_LT                 &ak_W           &ak_M           &ak_G           &ak_X
 #define HD_LM &mt_S LGUI S   &mt_C LALT C    &mt_N LSHFT N    &mt_T LCTRL T   &ak_K
 #define HD_LB &ak_F           &ak_P           &ak_L           &ak_D           
-#define HD_LH                                                 &lt_BSPC l_fun BSPC   &lt_R l_nav R
+#define HD_LH                                                 &lt_BSPC l_sym BSPC   &lt_R l_nav R
 
-#define HD_RT &MinusPlus      &DotColn        &SlshStar       &ak_J          
-#define HD_RM &CommaMagic     &mt_A RCTRL A   &mt_E RSHFT E    &mt_I RALT I    &mt_H LGUI H
+#define HD_RT &lk_LRDquote    &DotColn        &QuesExcl       &ak_J          
+#define HD_RM &CommaMagic     &mt_A RCTRL A   &mt_E RSHFT E   &mt_I RALT I    &mt_H LGUI H
 #define HD_RB                 &ak_U           &ak_O           &ak_Y           &ak_B
-#define HD_RH &lt l_sym SPACE &lt l_nav RETURN
+#define HD_RH &kp SPACE       &lt l_num RETURN
 
 
 #define l_akDOT_label "l_akDOT" // adaptive grabbag sticky layer.
