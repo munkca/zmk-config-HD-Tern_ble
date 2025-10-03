dabd983c6dae672b523d6a9df2fb871268df67ed
munkca <137710061+munkca@users.noreply.github.com>
2025-10-03 19:32:31 +0800

    Update vf-config.h

--- config/layouts/hd/vf-config.h
+++ config/layouts/hd/vf-config.h
@@ -34,7 +34,7 @@
 #define HD_RB                 &ak_U           &ak_O           &ak_Y           &ak_B
 #define HD_RH &kp SPACE       &lt l_num RETURN
 
-// aim to delete this layer, replacing it with better adaptive keys.
+/* aim to delete this layer, replacing it with better adaptive keys.
 #define l_akDOT_label "l_akDOT" // adaptive grabbag sticky layer.
 #define DOT_LT                 &kp T          &kp L           &kp L           &kp L    // Shifted one right from original layout. A problem?       
 #define DOT_LM _______        _______         _______         _______         _______
@@ -45,5 +45,5 @@
 #define DOT_RM &caps_word     _______         _______         _______         _______
 #define DOT_RB                _______         _______         _______         _______        
 #define DOT_RH _______        _______
-
+*/
 
